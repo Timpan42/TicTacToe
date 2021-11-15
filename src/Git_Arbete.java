57173f825dac930c1a929eb748ea988649dae0f4
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2021-11-15 11:16:50 +0100

    Kode börjar att skapas

--- src/Git_Arbete.java
+++ src/Git_Arbete.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class Git_Arbete {
+    public static  void main(String[] args){
+        /** Fråga efter hur många numer */
+        String numbers = JOptionPane.showInputDialog("How many numbers are you going to wright?");
+            int x = Integer.parseInt("numbers");
+            int y = 0;
+        /** Fråga efter vilka numer */
+        while (y>x);{
+
+        }
+
+    }
 }
