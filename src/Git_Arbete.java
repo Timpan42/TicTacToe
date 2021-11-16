f7e4912c737a81ce42b7abb64e9c607963e737ec
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2021-11-16 15:09:10 +0100

    lakdit lopp. Ska göra

--- src/Git_Arbete.java
+++ src/Git_Arbete.java
@@ -5,10 +5,14 @@ public class Git_Arbete {
         /** Fråga efter hur många numer */
         String numbers = JOptionPane.showInputDialog("How many numbers are you going to wright?");
             int x = Integer.parseInt("numbers");
-            int y = 0;
+            int y = 1;
         /** Fråga efter vilka numer */
         while (y>x);{
+            String num = JOptionPane.showInputDialog("with number? For space " + y);
+            int z = Integer.parseInt("num");
 
+
+            y =y+1;
         }
 
     }
