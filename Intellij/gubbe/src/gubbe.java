e65b2d450a5dd655da0539c6fae64921fb461346
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-01-17 11:17:38 +0100

    words

---
+++ Intellij/gubbe/src/gubbe.java
@@ -0,0 +1,32 @@
+public class gubbe {
+    public static void main(String args[]){
+    /* början fråga efter ett ord / ge ett random ord */
+    /* spara ordet */
+    String wordlist = "word";
+
+
+    /* della upp ordet var för sig */
+    String[] leter = wordlist.split("");
+
+
+    /* vissa ordet för spelaren i form av _ _ _ */
+
+
+
+    /* fråga spelare efter ett bokstav*/
+
+    /* spara bokstav*/
+
+    /* kolar vilken bokstaven är*/
+
+    /* om bokstav är rätt så vissas bokstaven av ordet. om fel, rita */
+
+    /* gör så det inte går att skriva bokstaven igen*/
+
+    /* vissa vad spelaren gisade */
+
+    /* loppa*/
+
+    /* fortsät tills spelaren van / förlorade */
+    }
+}
