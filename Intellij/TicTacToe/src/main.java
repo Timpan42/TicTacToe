cd708c52aa89c53e30cf70d8aecb9846da363868
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-02-22 15:19:00 +0100

    Försöker göra brädan

--- Intellij/TicTacToe/src/main.java
+++ Intellij/TicTacToe/src/main.java
@@ -1,7 +1,16 @@
 import javax.swing.*;
 
 public class main {
+
     public static void main(String args[]) {
 
+        char[][] bord = new char[3][3];
+
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++) {
+                bord[i][j] = '-';
+            }
+            }
+        System.out.print(bord[i][j]);
     }
 }
