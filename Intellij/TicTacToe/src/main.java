783e2a5e3acc264f740e931ed5f8b53450c1d4ef
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-03-01 15:10:21 +0100

    Bräda funka, den som vinner finns inte

--- Intellij/TicTacToe/src/main.java
+++ Intellij/TicTacToe/src/main.java
@@ -15,13 +15,14 @@ public class main {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Let's a go, Tic Tac Toe of deaf?");
-        System.out.print("Player 1, your name plez?");
-        String p1 = ("Bert");
-        System.out.print("Player 2, your name plez?");
-        String p2 = ("Gunhild");
+        System.out.print("Player 1, your name plez? ");
+        String p1 = in.nextLine();
+        System.out.print("Player 2, your name plez? ");
+        String p2 = in.nextLine();
 
         boolean player1 = true;
         boolean gameEnded = false;
+
         while (!gameEnded){
             drawBorde(bord);
             if (player1){
@@ -29,24 +30,47 @@ public class main {
             } else {
                 System.out.println(p2 + "'s Turn (o):" );
             }
-            char c = "-";
+            char c = '-';
             if (player1) {
-                c = "x";
+                c = 'x';
             } else {
-                c = "o";
+                c = 'o';
             }
             int x_led = 0;
             int y_led = 0;
 
             while (true) {
-                System.out.print("Enter a row number: ");
+                System.out.print("Enter a row number (0, 1, or 2): ");
                 x_led = in.nextInt();
-                System.out.print("Enter a column number: ");
+                System.out.print("Enter a column number (0, 1 or 2): ");
                 y_led = in.nextInt();
+                if (x_led < 0 || x_led > 2 || y_led < 0 || y_led > 2){
+                    System.out.println("your cheating peace of s*it. try again");
+                } else if (bord[x_led][y_led] != '-'){
+                    System.out.println("Wrong move do a again");
+                } else {
+                    break;
+                }
+            }
+            bord[x_led][y_led] = c;
+
+            if (playerWon(bord) == 'x'){
+                System.out.println(p1 + " Has won. " + p2 + " bad!");
+                gameEnded = true;
+            } else if (playerWon(bord) == 'o'){
+                System.out.println(p2 + " Has won. " + p1 + " bad!");
+                gameEnded = true;
+            } else {
+                if (gameTie(bord)){
+                    System.out.println("How did a tie happen?");
+                    gameEnded = true;
+                } else {
+                    player1 = !player1;
+                }
             }
         }
+        drawBorde(bord);
     }
-}
 
     public static void drawBorde(char[][] bord) {
         System.out.println("bord:");
@@ -54,6 +78,7 @@ public class main {
             for (int j = 0; j < bord[i].length; j++){
                 System.out.print(bord[i][j]);
             }
+            System.out.println();
         }
     }
 }
